116c2819347659bd95d7cdbb7aca582d112fd13f
ghaber96 <ghaber@sas.upenn.edu>
2017-05-26 17:18:45 -0400

    Update dThetadR.m

--- dThetadR.m
+++ dThetadR.m
@@ -1,4 +1,4 @@
-function [deriv] = dThetadR(nuclei, electrons, alpha)
+function [deriv] = dThetadR(system, alpha)
 % Calculate dTheta/dR using fourth order finite differencing methods. Seems
 % sort of inefficient, ask Joe if there is a better way to do this...
 global paramObj
@@ -10,10 +10,10 @@ lengthC = length(coeff);
 reC = (lengthC + 1) / 2;
 for j = 1:dim
     for i = 1:lengthC
-        loopNuc = nuclei;
-        loopNuc(alpha).pos(j, 1) = loopNuc(alpha).pos(j, 1) + ...
+        loopSys = system;
+        loopSys.nuclei(alpha).pos(j) = loopNuc.nuclei(alpha).pos(j) + ...
             (i - reC) * diff;
-        deriv(j) = deriv(j) + coeff(i) * calcTheta(loopNuc, electrons) / diff;
+        deriv(j) = deriv(j) + coeff(i) * calcTheta(loopSys) / diff;
     end
 end
 
