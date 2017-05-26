03b2c07a60cab4f01b4f02bec23155a7a4776820
ghaber96 <ghaber@sas.upenn.edu>
2017-05-26 17:20:22 -0400

    Update calcTheta.m

--- calcTheta.m
+++ calcTheta.m
@@ -1,6 +1,6 @@
-function [theta] = calcTheta(nuclei, electrons)
+function [theta] = calcTheta(system)
 % Calculate theta based on gamma
-gamma = calcGamma(nuclei, electrons);
+gamma = calcGamma(system);
 theta = real(trace(gamma));
 end
 
