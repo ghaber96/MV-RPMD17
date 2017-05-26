bd496358d64b50dd7e696c4080e628a2779d32eb
ghaber96 <ghaber@sas.upenn.edu>
2017-05-26 17:02:30 -0400

    Update updNucMom.m

--- updNucMom.m
+++ updNucMom.m
@@ -1,21 +1,18 @@
-function [oNucleus] = updNucMom(iNuc)
+function [oNucleus] = updNucMom(systemTot, alpha)
 % update the nuclear momentum
 global paramObj
 
 coeff = MyConstants.ADAMS_FOURTH;
-
-oNucleus = iNuc;
 pre2 = paramObj.ringSize / paramObj.beta;
 pre1 = - pre2 * paramObj.massNuc / paramObj.beta;
 
-for j = 1:length(coeff)
-    oNucleus.mom(:, 1) = oNucleus.mom(:, 1) + timeStep * coeff(j) / 24 * ...
-        (pre1 * oNucleus.accel(:, j) - oNucleus.dVndR(:, j) + pre2 /...
-        oNucleus.theta(j) * oNucelus.dThdR(:, j));
-end
+oNucleus = systemTot(1).nuclei(alpha);
 
-for u = 1:(length(coeff) - 1)
-    oNucleus.mom(:, u + 1) = iNuc.mom(:, u);
+for time = 1:length(coeff)
+    oNucleus.mom(:) = oNucleus.mom(:) + timeStep * coeff(time) / 24 * ...
+        (pre1 * systemTot(time).nuclei(alpha).accel(:) - ...
+        systemTot(time).nuclei(alpha).dVndR(:) + pre2 /...
+        systemTot(time).theta * systemTot(time).nuclei(alpha).dThdR(:));
 end
 end
 
